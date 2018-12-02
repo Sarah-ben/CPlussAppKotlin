2bf2a9da4ef612ba67bf9d1f1ff83cbc8b04617e
Mac Pro <macpro@Macs-MacBook-Pro.local>
2018-12-02 22:38:09 +0200

    Initial commit

---
+++ app/src/main/java/com/example/macpro/myapp/HomeActivity.kt
@@ -0,0 +1,85 @@
+package com.example.macpro.myapp
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.app_bar_home.*
+
+class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        setSupportActionBar(toolbar)
+        if(savedInstanceState == null){
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.home_frame,HomeFragment())
+            .commitNow()
+        }
+        window.decorView.layoutDirection = View.LAYOUT_DIRECTION_RTL
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.home -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.home_frame,HomeFragment())
+                    .commitNow()
+            }
+            R.id.about -> {
+                     supportFragmentManager.beginTransaction()
+                    .replace(R.id.home_frame,AboutFragment())
+                    .commitNow()
+            }
+            R.id.team -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.home_frame,TeamFragment())
+                    .commitNow()
+            }
+            R.id.order -> {
+            startActivity(Intent(Intent.ACTION_VIEW,
+                Uri.parse("mailto:" + "info@ali-gamal.com" + "?subject=" + " رسالة من التطبيق")))
+            }
+            R.id.nav_share -> {
+                val sent = Intent()
+                sent.action = Intent.ACTION_SEND
+                sent.putExtra(Intent.EXTRA_TEXT , "https://play.google.com/store/apps/details?id=com.example.macpro.myapp حمل تطبيق تعلم السي بلس بلس ")
+                sent.type = "text/plain"
+                startActivity(Intent.createChooser(sent,"إختار التطبيق الذي تريد المشاركه معه:"))
+            }
+
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+}
